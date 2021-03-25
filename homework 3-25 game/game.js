88c0dd5609e23c794b76a1cc8cdb33b2dbd73247
Skye Finley <80650005+Skye-Finley@users.noreply.github.com>
2021-03-25 03:31:19 -0500

    Add files via upload

---
+++ homework 3-25 game/game.js
@@ -0,0 +1,14 @@
+let test = prompt("What's your name?");
+
+
+
+var box=document.getElementById("box");
+var yes=document.getElementById("yes");
+var no=document.getElement("no");
+
+yes.onclick=function(){
+box.style.backgroundColor-"red";
+}
+no.onclick=function(){
+box.style.backgroundColor="green"
+}
